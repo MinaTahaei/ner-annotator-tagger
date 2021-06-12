997c8fb8b9d39f2348d36fae611cb22d8a39476f
Mohammad Hossein Sohi <mh.hashemi79@gmail.com>
2021-06-12 12:01:29 +0430

    PDF-1

---
+++ ner-annotator/ui/vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    chainWebpack: config => {
+        config.module
+        .rule("pdf")
+        .test(/\.pdf$/)
+        .use("file-loader")
+        .loader("file-loader");
+    }
+}
